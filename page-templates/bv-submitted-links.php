4a9fc22871320e965588f08a886454e87b63a9a0
Paul Flannery <psflannery@gmail.com>
2015-06-30 23:22:10 +0100

    prepare for post submissions
    A little tidy up of the form styling in preparation of the user submit
    plugin for Ben's Reading Section

---
+++ page-templates/bv-submitted-links.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Template Name: Submitted Links - Ben Vickers
+ * Description: The template to display the links submitted by users in response to Ben Vickers' reading section.
+ *
+ * @package Opening Times
+ */
+ 
+get_header(); ?>
+
+    <main id="main" class="site-main accordion-issue container-fluid" role="main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <?php get_template_part( 'template-parts/single', 'issue' ); ?>
+
+            <?php endwhile; ?>
+
+            <?php the_posts_navigation(); ?>
+
+        <?php else : ?>
+
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+        <?php endif; ?>
+
+    </main>
+
+<?php get_footer(); ?>
