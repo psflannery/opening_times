83b15f0e8daaebc674e43a5c6b4b3e2a1c9e77f0
psflannery <psflannery@gmail.com>
2015-08-19 21:16:25 +0100

    jetpack inf scroll

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Opening Times
 The theme for the main Opening Times website.
 http://otdac.org/
 
-Version: 1.3.3
+Version: 1.3.4
 Date: 18/8/15
 
 Contains the main theme files responsible for presentation. All theme agnostic functionality like custom post-types and taxanomies are located in a separate plugin.
--- inc/jetpack.php
+++ inc/jetpack.php
@@ -13,7 +13,18 @@
 function opening_times_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
+		'render'    => 'opening_times_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
 }
 add_action( 'after_setup_theme', 'opening_times_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function opening_times_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+}
